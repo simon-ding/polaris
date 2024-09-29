ce25c090f57cb787956e190e903cea1fcb5549a4
Simon Ding <dingchuan91@gmail.com>
2024-09-29 13:55:16 +0800

    feat: open url at startup

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"polaris/db"
 	"polaris/log"
+	"polaris/pkg/utils"
 	"polaris/server"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -17,6 +19,13 @@ func main() {
 		log.Panicf("init db error: %v", err)
 	}
 
+	go func() {
+		time.Sleep(2 * time.Second)
+		if err := utils.OpenURL("http://127.0.0.1:8080"); err != nil {
+			log.Errorf("open url error: %v", err)
+		}
+
+	}()
 	s := server.NewServer(dbClient)
 	if err := s.Serve(); err != nil {
 		log.Errorf("server start error: %v", err)
