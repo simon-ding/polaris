f77f565146d2650f55160b0393bfe0b7021fa9bc
Simon Ding <dingchuan91@gmail.com>
2024-10-10 18:16:34 +0800

    fix: add user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+groupadd -g ${PGID} abc
+useradd abc -u ${PUID} -g ${PGID} -m -s /bin/bash
+
 ## 重设权限
-chown -R "${PUID}:${PGID}" /app/data
+chown -R abc:abc /app/data
 
 umask ${UMASK:-022}
 
 cd /app
-exec gosu "${PUID}:${PGID}" /app/polaris
+exec gosu chown -R abc:abc /app/polaris
