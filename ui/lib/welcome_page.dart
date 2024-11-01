2da02fa7062f996c932f05b2cf31ac0ca86edef3
Simon Ding <dingchuan91@gmail.com>
2024-11-01 18:14:32 +0800

    ui: add filter option

--- ui/lib/welcome_page.dart
+++ ui/lib/welcome_page.dart
@@ -7,52 +7,133 @@ import 'package:ui/providers/welcome_data.dart';
 import 'package:ui/tv_details.dart';
 import 'package:ui/widgets/progress_indicator.dart';
 
-class WelcomePage extends ConsumerWidget {
+class WelcomePage extends ConsumerStatefulWidget {
+  const WelcomePage({super.key});
   static const routeTv = "/series";
   static const routeMoivie = "/movies";
 
-  const WelcomePage({super.key});
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return WelcomePageState();
+  }
+}
+
+class WelcomePageState extends ConsumerState<WelcomePage> {
+  //WelcomePageState({super.key});
+
+  bool onlyShowUnfinished = false;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     var uri = GoRouterState.of(context).uri.toString();
 
     AsyncValue<List<MediaDetail>> data;
-    if (uri == routeMoivie) {
+    if (uri == WelcomePage.routeMoivie) {
       data = ref.watch(movieWatchlistDataProvider);
     } else {
       data = ref.watch(tvWatchlistDataProvider);
     }
 
-    return switch (data) {
-      AsyncData(:final value) => SingleChildScrollView(
-          child: Wrap(
-            alignment: WrapAlignment.start,
-            spacing: isSmallScreen(context) ? 0 : 10,
-            runSpacing: isSmallScreen(context) ? 10 : 20,
-            children: value.isEmpty
-                ? [
-                    Container(
-                        height: MediaQuery.of(context).size.height * 0.6,
-                        alignment: Alignment.center,
-                        child: const Text(
-                          "啥都没有...",
-                          style: TextStyle(fontSize: 16),
-                        ))
-                  ]
-                : List.generate(value.length, (i) {
-                    final item = value[i];
-                    return MediaCard(item: item);
-                  }),
-          ),
+    return Stack(
+      //alignment: Alignment.bottomRight,
+      children: [
+        () {
+          return switch (data) {
+            AsyncData(:final value) => SingleChildScrollView(
+                child: Wrap(
+                  alignment: WrapAlignment.start,
+                  spacing: isSmallScreen(context) ? 0 : 10,
+                  runSpacing: isSmallScreen(context) ? 10 : 20,
+                  children: getMediaAll(value),
+                ),
+              ),
+            _ => const MyProgressIndicator(),
+          };
+        }(),
+        Row(
+          children: [
+            Expanded(child: Container()),
+            Column(
+              children: [
+                Expanded(child: Container()),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: MenuAnchor(
+                    style: MenuStyle(
+                      //minimumSize: WidgetStatePropertyAll(Size(400, 300)),
+
+                      backgroundColor: WidgetStatePropertyAll(Theme.of(context)
+                          .colorScheme
+                          .inversePrimary
+                          .withOpacity(0.9)),
+                    ),
+                    menuChildren: [
+                      MenuItemButton(
+                        onPressed: null,
+                        child: CheckboxListTile(
+                          value: onlyShowUnfinished,
+                          onChanged: (b) {
+                            setState(() {
+                              onlyShowUnfinished = b!;
+                            });
+                          },
+                          title: const Text(
+                            "未完成",
+                            style: TextStyle(fontSize: 16),
+                            softWrap: false,
+                          ),
+                          controlAffinity: ListTileControlAffinity.leading,
+                        ),
+                      ),
+                    ],
+                    builder: (context, controller, child) {
+                      return Opacity(
+                          opacity: 0.7,
+                          child: FloatingActionButton(
+                            onPressed: () {
+                              if (controller.isOpen) {
+                                controller.close();
+                              } else {
+                                controller.open();
+                              }
+                            },
+                            child: const Icon(Icons.more_horiz),
+                          ));
+                    },
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
-      _ => const MyProgressIndicator(),
-    };
+      ],
+    );
   }
 
   bool isSmallScreen(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     return screenWidth < 600;
+  }
+
+  List<Widget> getMediaAll(List<MediaDetail> list) {
+    if (list.isEmpty) {
+      return [
+        Container(
+            height: MediaQuery.of(context).size.height * 0.6,
+            alignment: Alignment.center,
+            child: const Text(
+              "啥都没有...",
+              style: TextStyle(fontSize: 16),
+            ))
+      ];
+    }
+    if (onlyShowUnfinished) {
+      list = list.where((v) => v.downloadedNum != v.monitoredNum).toList();
+    }
+    return List.generate(list.length, (i) {
+      final item = list[i];
+      return MediaCard(item: item);
+    });
   }
 }
 
