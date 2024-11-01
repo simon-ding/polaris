0b1bd8226dbe287e4de5d2f317aaa470fd5a81b2
Simon Ding <dingchuan91@gmail.com>
2024-11-01 22:00:18 +0800

    fix: add validator

--- ui/lib/welcome_page.dart
+++ ui/lib/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ui/movie_watchlist.dart';
 import 'package:ui/providers/APIs.dart';
@@ -186,6 +187,7 @@ class WelcomePageState extends ConsumerState<WelcomePage> {
                   FormBuilderTextField(
                     name: "name",
                     decoration: InputDecoration(labelText: "要解析的名字"),
+                    validator: FormBuilderValidators.required(),
                   ),
                   FormBuilderDropdown(
                     name: "type",
