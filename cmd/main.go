9eb8278f788c4929e7e59c4e4527c97dbb0dd828
Simon Ding <dingchuan91@gmail.com>
2025-04-09 11:01:37 +0800

    feat: export C lib

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,6 @@
 package main
 
+import "C"
 import (
 	"polaris/db"
 	"polaris/log"
@@ -7,22 +8,18 @@ import (
 )
 
 func main() {
-	log.Infof("------------------- Starting Polaris ---------------------")
+	Start()
+}
 
-	//utils.MaxPermission()
+//export Start
+func Start() {
+	log.Infof("------------------- Starting Polaris ---------------------")
 
 	dbClient, err := db.Open()
 	if err != nil {
 		log.Panicf("init db error: %v", err)
 	}
 
-	// go func() {
-	// 	time.Sleep(2 * time.Second)
-	// 	if err := utils.OpenURL("http://127.0.0.1:8080"); err != nil {
-	// 		log.Errorf("open url error: %v", err)
-	// 	}
-
-	// }()
 	s := server.NewServer(dbClient)
 	if err := s.Serve(); err != nil {
 		log.Errorf("server start error: %v", err)
